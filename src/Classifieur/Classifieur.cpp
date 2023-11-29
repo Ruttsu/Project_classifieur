53958cda7da911888f0b31ca6dc7bd8b1ab511f7
GUYOT.L <luckidark777@gmail.com>
2023-11-29 09:04:35 +0100

    Suppression du build du commit precedant

---
+++ src/Classifieur/Classifieur.cpp
@@ -0,0 +1,12 @@
+﻿// Classifieur.cpp : définit le point d'entrée de l'application.
+//
+
+#include "Classifieur.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
