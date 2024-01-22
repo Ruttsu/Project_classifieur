bee5cd54be109755768afc2972b71f46c5e0ecad
GUYOT.L <luckidark777@gmail.com>
2024-01-22 14:09:54 +0100

    debut d'implementation pour les commandes

--- src/Classifieur/Classifieur.cpp
+++ src/Classifieur/Classifieur.cpp
@@ -10,6 +10,15 @@
 
 using namespace std;
 
+void afficherMenu()
+{
+    cout << "Commandes disponibles (entrer la numéro de la commande):" << endl;
+    cout << "1. Définir les fichiers d'entrer" << endl;
+    cout << "2. Choisir la valeur de k <valeur>" << endl;
+    cout << "3. Afficher les résultats" << endl;
+    cout << "0. Quitter" << endl;
+}
+
 int main() {
 /*
     Data file("D:/Cours/INSA/Cpp/ProjetClassifieur/doc/Digits/Digits/digits.svm", 1);
@@ -69,6 +78,51 @@ int main() {
 	cout << DataCosine << " est le plus proche voisin de Test !" << endl;
 */
 
+
+    int commande;
+    //donner a mettre dans les bonnes class
+    int k;
+    string path;
+    bool donneeApprentissage;
+
+    do {
+        afficherMenu();
+        cout << "Entrez une commande : ";
+        cin >> commande;
+
+        switch (commande) {
+            case 1 :
+                cout << "Entrez le chemin vers le fihchier que vous voulez entrer" << endl;
+                cin >> path;
+                cout << "Entrez 1 si c'est un fichier d'apprenttissage 0 si c'est un fichier a tester" << endl;
+                cin >> donneeApprentissage;
+                //creation d'une class data
+                //Data mettreUnNom(path, donneeApprentissage);
+                break;
+
+            case 2 :
+                cout << "Entrez la valeur de k : ";
+                cin >> k;
+                cout << "La valeur de k est : " << k << endl;
+                break;
+
+            case 3 :
+
+                break;
+
+            default :
+                cout << "Commande inconnue." << endl;
+        }
+
+
+
+    } while (commande != 0);
+
+    cout << "Programme terminé." << endl;
+
+
+
+/* TEST de la class classificationReport
 ClassificationReport test;
 
 test.displayTabConfusion();
@@ -86,7 +140,7 @@ test.displayTabConfusion();
     test.setTabConfusion(1,1);
     test.setTabConfusion(1,1);
 
-test.displayTabConfusion();
-
+    test.displayTabConfusion();
+*/
 	return 0;
 }
