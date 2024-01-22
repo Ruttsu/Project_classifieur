478bc8f6aeb3aa6acef985d9e91d2b5f2b6fdb36
GUYOT.L <luckidark777@gmail.com>
2024-01-22 17:30:27 +0100

    tampon

--- src/Classifieur/Classifieur.cpp
+++ src/Classifieur/Classifieur.cpp
@@ -21,62 +21,17 @@ void afficherMenu()
 
 int main() {
 
-    Data file("D:/Cours/INSA/Cpp/ProjetClassifieur/doc/Digits/Digits/digits.svm", 1);
+
+    string path;
+    cout << "Donnez le chemin du fichiers d'entrainement :" << endl;
+    cin >> path;
+    Data dataApprentissage(path,1);
+
+    dataApprentissage.displayData();
+
+   /* Data file("D:/Cours/INSA/Cpp/ProjetClassifieur/doc/Digits/Digits/digits.svm", 1);
     file.displayData();
-    cout << "Fini de l'import" << endl << endl;
-
-
-	Sample test, data1, data2;
-	KnnEuclidien A;
-	KnnManhattan B;
-	KnnCosine C;
-
-	data1.addFeatures(200);	// avec 200 = resultat sympa !
-	data1.addFeatures(19);
-	data1.addFeatures(18);
-	data1.addFeatures(17);
-
-	data2.addFeatures(150);
-	data2.addFeatures(-15);
-	data2.addFeatures(18);
-	data2.addFeatures(1);
-
-	test.addFeatures(21);
-	test.addFeatures(20);
-	test.addFeatures(19);
-	test.addFeatures(18);
-
-	cout << "Data1 : ";
-	data1.displayFeatureVector();
-	cout << "Data2 : ";
-	data2.displayFeatureVector();
-	cout << "Test : ";
-	test.displayFeatureVector();
-	cout << endl;
-
-	cout << "Méthode Euclidienne : " << endl;
-	cout << "Distance Data1 & Test : " << A.similarity(test,data1) << endl;
-	cout << "Distance Data2 & Test : " << A.similarity(test,data2) << endl;
-
-	string DataEuclidien = (A.similarity(test,data1) < A.similarity(test,data2))? "Data1":"Data2";
-	cout << DataEuclidien << " est le plus proche voisin de Test !" << endl << endl;
-
-
-	cout << "Méthode Manhattan : " << endl;
-	cout << "Distance Data1 & Test : " << B.similarity(test,data1) << endl;
-	cout << "Distance Data2 & Test : " << B.similarity(test,data2) << endl;
-
-	string DataManhatan = (B.similarity(test,data1) < B.similarity(test,data2))? "Data1":"Data2";
-	cout << DataManhatan << " est le plus proche voisin de Test !" << endl << endl;
-
-
-	cout << "Méthode Cosine : " << endl;
-	cout << "Distance Data1 & Test : " << C.similarity(test,data1) << endl;
-	cout << "Distance Data2 & Test : " << C.similarity(test,data2) << endl;
-
-	string DataCosine = (C.similarity(test,data1) > C.similarity(test,data2))? "Data1":"Data2";
-	cout << DataCosine << " est le plus proche voisin de Test !" << endl;
-
+    */
 /*
     int commande;
     //donner a mettre dans les bonnes class
